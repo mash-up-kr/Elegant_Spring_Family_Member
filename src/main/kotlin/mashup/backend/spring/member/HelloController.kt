1436ca77cf27c2eee30cc8fffcb2e45ea1557c68
Haeseong Jeon <seong0428@gmail.com>
2021-08-01 15:09:40 +0900

    Add hello api

---
+++ src/main/kotlin/mashup/backend/spring/member/HelloController.kt
@@ -0,0 +1,10 @@
+package mashup.backend.spring.member
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    fun hello(): String = "Hello, Member-api"
+}
