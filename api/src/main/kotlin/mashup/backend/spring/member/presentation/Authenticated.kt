1ea1a2564a638d3905b07a47e8ba0d6308950c08
전해성 <seong0428@gmail.com>
2021-08-22 16:14:45 +0900

    feat: annotation 추가

---
+++ api/src/main/kotlin/mashup/backend/spring/member/presentation/Authenticated.kt
@@ -0,0 +1,5 @@
+package mashup.backend.spring.member.presentation
+
+@Target(AnnotationTarget.TYPE, AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Authenticated
